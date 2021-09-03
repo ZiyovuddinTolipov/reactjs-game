319769ddb7819442f3e557251bcc67bb3883814c
Ziyovuddin <ziyovuddinweb@gmail.com>
2021-09-03 12:01:41 +0500

    React.js GaMe

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';   //  npm install --save react-router-dom
+import App from './App.js';
+
+
+ReactDOM.render(
+
+    <App />
+  ,
+  document.getElementById('root')
+);
